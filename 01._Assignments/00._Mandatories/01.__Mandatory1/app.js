824d6f08879c142b56ae337e084aa33141669b25
realkoder <alex8970@stud.kea.dk>
2024-03-01 21:15:32 +0100

    Prepared boilerplate for configuration of mandatory with homepage html, css and express server for serving static content and sending files for client rendering

--- 01._Assignments/00._Mandatories/01.__Mandatory1/app.js
+++ 01._Assignments/00._Mandatories/01.__Mandatory1/app.js
@@ -1,1 +1,28 @@
-import express from "express"
+import express from "express";
+import path from "path"
+
+const app = express();
+
+app.use(express.static("public"));
+
+
+// ======================= HTML =======================
+
+// Serving index / homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/pages/homepage/homepage.html"));
+});
+
+// ======================= API =======================
+
+
+
+const PORT = 8080;
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log("Server FAILED running on: ", PORT);
+    } else {
+        console.log("Server is running on port: ", PORT);
+    }
+})
