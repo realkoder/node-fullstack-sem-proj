83155888abcc158c0bb79600cc91514bbbbfb932
realkoder <alex8970@stud.kea.dk>
2024-03-20 22:46:54 +0100

    Trying again to deploy on vercel

--- 01._Assignments/00._Mandatories/01.__Mandatory1/app.js
+++ 01._Assignments/00._Mandatories/01.__Mandatory1/app.js
@@ -1,25 +1,25 @@
-import express from "express";
+    import express from "express";
 
-const app = express();
+    const app = express();
 
-app.use(express.static("public"));
+    app.use(express.static("public"));
 
 
-// ======================= HTML =======================
-import pagesRouter from "./routers/pagesRouter.js"
-app.use(pagesRouter);
+    // ======================= HTML =======================
+    import pagesRouter from "./routers/pagesRouter.js"
+    app.use(pagesRouter);
 
-// ======================= API =======================
-import markdownDataApiRouter from "./routers/api/markdownData.js"
-app.use(markdownDataApiRouter);
+    // ======================= API =======================
+    import markdownDataApiRouter from "./routers/api/markdownData.js"
+    app.use(markdownDataApiRouter);
 
-const PORT = 8080;
+    const PORT = 8080;
 
-app.listen(PORT, (error) => {
-    if (error) {
-        console.log("Server FAILED running on: ", PORT);
-    } else {
-        console.log("Server is running on port: ", PORT);
-        console.log("Go to: ", new URL(`http://localhost:${PORT}/`).href);
-    }
-});
+    app.listen(PORT, (error) => {
+        if (error) {
+            console.log("Server FAILED running on: ", PORT);
+        } else {
+            console.log("Server is running on port: ", PORT);
+            console.log("Go to: ", new URL(`http://localhost:${PORT}/`).href);
+        }
+    });
