b16cca2a88bc4dbc2066e99b749835f16fdca9bb
realkoder <alex8970@stud.kea.dk>
2024-03-22 11:47:30 +0100

    Added component greeter and friendscounter for 07svelte project

--- 07._First_Svelte_Project/src/App.svelte
+++ 07._First_Svelte_Project/src/App.svelte
@@ -1,9 +1,10 @@
 <script>
-    import Greeter from "./components/Greeter/Greeter.svelte";
-
-  
+  import FriendsCounter from "./components/FriendsCounter/FriendsCounter.svelte";
+  import Greeter from "./components/Greeter/Greeter.svelte";
 </script>
 
 <main>
   <Greeter name="BOB"></Greeter>
+
+  <FriendsCounter friendsCounter={0}></FriendsCounter>
 </main>
---
+++ 07._First_Svelte_Project/src/components/FriendsCounter/FriendsCounter.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let friendsCounter = 0;
+    function addFriend() {
+        friendsCounter += 1;
+    }
+
+    function alienateFriend() {
+        friendsCounter--;           
+    }
+</script>
+
+<div>
+    <h2>
+        {friendsCounter === 0
+            ? "You have none friends :'("
+            : `You have ${friendsCounter} friends`}
+    </h2>
+    <button on:click={addFriend}>Add one more friend</button>
+    <button on:click={alienateFriend}>Alienate a friend</button>
+</div>
